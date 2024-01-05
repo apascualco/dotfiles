4f6866cd8534d8560bb7e9e3abecd9d10b970aaa
apascualco <apascualco@gmail.com>
2024-01-05 16:43:59 +0100

    Test fugitive and gitgutter

--- ides/nvim/lua/apascualco/set.lua
+++ ides/nvim/lua/apascualco/set.lua
@@ -4,7 +4,7 @@ local set = vim.keymap.set
 g.mapleader = ','
 
 set("n", "ghs", "<Plug>(GitGutterStageHunk)")
-set("n", "ghu", "<Plug>(GitGutterUndoHunk)")
+set("n", "ghu", "<Plug>(GitGutterUndoHunk)", { desc = "Rollback change" })
 
 set("n", ">>", ":bn<CR>")
 set("n", "<<", ":bp<CR>")
