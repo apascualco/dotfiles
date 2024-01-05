152b87c64a8f9b91ca2687389c2a90fce0dc6d92
apascualco <apascualco@gmail.com>
2024-01-05 16:55:51 +0100

    Add description into stage hunk

--- ides/nvim/lua/apascualco/set.lua
+++ ides/nvim/lua/apascualco/set.lua
@@ -3,7 +3,7 @@ local set = vim.keymap.set
 
 g.mapleader = ','
 
-set("n", "ghs", "<Plug>(GitGutterStageHunk)")
+set("n", "ghs", "<Plug>(GitGutterStageHunk)", { desc = "Add to stage the line hover" })
 set("n", "ghu", "<Plug>(GitGutterUndoHunk)", { desc = "Rollback change" })
 
 set("n", ">>", ":bn<CR>")
