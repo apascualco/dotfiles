ffe713e204609f81101ed6d5cfc9507a9ca9f3b7
apascualco <apascualco@gmail.com>
2024-01-05 21:24:12 +0100

    Update lazy installs and add octo

--- ides/nvim/lua/apascualco/lazy.lua
+++ ides/nvim/lua/apascualco/lazy.lua
@@ -4,21 +4,21 @@ require("lazy").setup({
 
   -- Theme apparence
 	'nvim-tree/nvim-web-devicons',
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
 
   -- Highlighting of code
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
 
   -- Mapper to telescope the shortcuts
   {
-    "lazytanuki/nvim-mapper",
-    config = function() require("nvim-mapper").setup{} end,
+    'lazytanuki/nvim-mapper',
+    config = function() require('nvim-mapper').setup{} end,
   },
 
   -- Diagnostics
   {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {},
   },
 
@@ -27,11 +27,11 @@ require("lazy").setup({
     'nvim-telescope/telescope.nvim',
     dependencies = {
       {'nvim-lua/plenary.nvim'},
-      { "benfowler/telescope-luasnip.nvim" }
+      { 'benfowler/telescope-luasnip.nvim' }
     },
-    config = function() 
-      require("telescope").load_extension("mapper")
-      require("telescope").load_extension("luasnip")
+    config = function()
+      require('telescope').load_extension('mapper')
+      require('telescope').load_extension('luasnip')
     end
   },
   -- browse the tags 
@@ -44,16 +44,16 @@ require("lazy").setup({
 	{ 'hrsh7th/cmp-path' },
   { 'hrsh7th/cmp-cmdline' },
 	{ 'hrsh7th/nvim-cmp' },
-  
+
   -- LSP installer
-  { "williamboman/mason.nvim" },
+  { 'williamboman/mason.nvim' },
 
   -- Snippers
   { 'L3MON4D3/LuaSnip' },
   { 'saadparwaiz1/cmp_luasnip' },
   { 'hrsh7th/cmp-vsnip' },
   { 'hrsh7th/vim-vsnip' },
-  { 'SirVer/ultisnips' }, 
+  { 'SirVer/ultisnips' },
   { 'quangnguyen30192/cmp-nvim-ultisnips' },
   { 'dcampos/nvim-snippy' },
   { 'dcampos/cmp-snippy' },
@@ -79,7 +79,7 @@ require("lazy").setup({
   -- Debug adapter protocol
   { 'mfussenegger/nvim-dap' },
   { 'theHamsta/nvim-dap-virtual-text' },
-  { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap"} },
+  { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap' } },
 
   -- IA
   { 'jpmcb/nvim-llama' },
@@ -89,10 +89,54 @@ require("lazy").setup({
 
   { 'dstein64/vim-startuptime' },
 
+  -- Terminal
   {
-    "willothy/flatten.nvim",
+    'willothy/flatten.nvim',
     config = true,
     lazy = false,
     priority = 1001,
   },
+
+  -- Surround shortcuts
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+        require('nvim-surround').setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+
+  -- Underline same words
+  {
+    'sontungexpt/stcursorword',
+    event = 'VeryLazy',
+    config = true,
+  },
+  {
+    'pwntester/octo.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function ()
+      require"octo".setup()
+    end
+  }
 })
+
+--require('k8s').setup({})
+
+
+--     Old text                    Command         New text
+--------------------------------------------------------------------------------
+--    surr*ound_words             ysiw)           (surround_words)
+--    *make strings               ys$"            "make strings"
+--    [delete ar*ound me!]        ds]             delete around me!
+--    remove <b>HTML t*ags</b>    dst             remove HTML tags
+--    'change quot*es'            cs'"            "change quotes"
+--    <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+--    delete(functi*on calls)     dsf             function calls
