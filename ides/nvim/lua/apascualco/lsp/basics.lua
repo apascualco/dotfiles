de3984c81881395cf1b08307a28e0f43723cfa14
apascualco <apascualco@gmail.com>
2024-04-07 10:10:46 +0200

    Remove some hardcode homes

---
+++ ides/nvim/lua/apascualco/lsp/basics.lua
@@ -0,0 +1,27 @@
+local lspconfig = require('lspconfig')
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig.cssls.setup({
+  capabilities = capabilities
+})
+
+lspconfig.tailwindcss.setup({
+  capabilities = capabilities
+})
+
+lspconfig.terraformls.setup({
+  capabilities = capabilities
+})
+
+lspconfig.tsserver.setup({
+  capabilities = capabilities
+})
+
+lspconfig.gradle_ls.setup({
+  capabilities = capabilities
+})
+
+lspconfig.graphql.setup({
+  capabilities = capabilities
+})
+
